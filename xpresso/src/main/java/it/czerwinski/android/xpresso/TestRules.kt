1b444f38a632adc1c8a838ec1cda8334d621287c
Slawomir Czerwinski <slawomir@czerwinski.it>
2018-04-08 17:37:51 +0100

    ✨ intentsTestRule<T>()

--- xpresso/src/main/java/it/czerwinski/android/xpresso/TestRules.kt
+++ xpresso/src/main/java/it/czerwinski/android/xpresso/TestRules.kt
@@ -2,10 +2,14 @@ package it.czerwinski.android.xpresso
 
 import android.app.Activity
 import android.content.Intent
+import android.support.test.espresso.intent.rule.IntentsTestRule
 import android.support.test.rule.ActivityTestRule
 
 inline fun <reified T : Activity> activityTestRule(initialTouchMode: Boolean = false, launchActivity: Boolean = true) =
         ActivityTestRule(T::class.java, initialTouchMode, launchActivity)
+
+inline fun <reified T : Activity> intentsTestRule(initialTouchMode: Boolean = false, launchActivity: Boolean = true) =
+        IntentsTestRule(T::class.java, initialTouchMode, launchActivity)
 
 fun <T : Activity> ActivityTestRule<T>.launchActivity(intentInit: Intent.() -> Unit = {}): T = Intent()
         .apply(intentInit)
